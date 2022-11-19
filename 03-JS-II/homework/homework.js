a48183a9cfa5fa86d93cde6c28a45d834e820bdb
nicoSaffarano1995 <you@example.com>
2022-11-19 15:35:20 -0300

    "v2"

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -150,18 +150,17 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
-  if ((num1 > num2) && (num1 > num3) && (num1 > 0)){
-    return "Número 1 es mayor y positivo";
-  }
-  else if ((num1 < 0) || (num2 < 0) || (num3 < 0)){
+  if(num1 < 0 || num2 < 0 || num3 < 0) {
     return "Hay negativos";
   }
-  else if ( (num3 > num1) && (num3 > num2)){
-    num3++;
-    return num3;
-  }
-  else if ((num1 === 0) || (num2 === 0) || (num3 === 0)){
+  else if(num1 === 0 || num2 === 0 || num3 === 0) {
     return "Error";
+  }
+  else if(num1 > 0 && num1 > num2 && num1 > num3) {
+    return "Número 1 es mayor y positivo";
+  }
+  else if(num3 > num1 && num3 > num2) {
+    return num3 + 1;
   }
   else {
     return false;
@@ -174,24 +173,24 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-  if ( numero < 2) return false;
+  if( numero < 2) return false;
   if(numero === 2) return true;
-  for (var i = 2; i < numero; i++){
-    if(numero % i === 0){
+  for(var i = 2; i < numero; i++) {
+    if(numero % i === 0) {
       return false;
     }
-    return true;
   }
+  return true;
 }
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
-  if(true){
-    return "Soy Verdadero";
+  if(valor === true){
+    return "Soy verdadero";
   }
-  return "Soy Falso";
+  return "Soy falso";
 }
 
 function tablaDelSeis(){
