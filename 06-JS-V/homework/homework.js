4f7e2dcef3316fbbc7d211b9a020073f7286a2fe
nicoSaffarano1995 <you@example.com>
2022-12-12 16:57:37 -0300

    function line 79 done

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -80,7 +80,7 @@ function crearInstanciaPersona(nombre, apellido, edad, dir) {
   //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
   //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
   //Devolver la nueva persona creada
-  var juan = new Persona('Juan', 'Perez', 22, 'Saavedra 123')
+  var juan = new Persona(nombre,apellido,edad,dir);
 
   return juan;
 }
